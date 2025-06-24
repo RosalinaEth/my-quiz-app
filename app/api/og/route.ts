7c3b42d9166d796a3df3c2f256054184b103dc06
RosalinaEth <nellybellarosalina@gmail.comgit add .git commit -m Initial>
2025-06-24 17:06:19 +0100

    jpg

--- app/api/og/route.ts
+++ app/api/og/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
     <html>
       <head>
         <meta property="og:title" content="Which Element Are You?" />
-        <meta property="og:image" content="https://my-quiz-app-wff4.vercel.app/images/which-element-are-you.png" />
+        <meta property="og:image" content="https://my-quiz-app-wff4.vercel.app/images/which-element-are-you.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </head>
       <body>
