256cdfe145fec7e73f41dda8038cb6a0b4ae326f
RosalinaEth <nellybellarosalina@gmail.comgit add .git commit -m Initial>
2025-06-24 21:52:55 +0100

    Add OG image meta in head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Which Element Are You?</title>
+      <meta property="og:title" content="Which Element Are You?" />
+      <meta
+        property="og:image"
+        content="https://my-quiz-app-wff4.vercel.app/images/which-element-are-you.jpg"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+    </>
+  )
+}
